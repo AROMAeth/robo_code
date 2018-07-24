37f0e6751e7e7e7a1257fa59da8410ffab3981fa
Niklas Funk <nfunk@ethz.ch>
2018-07-24 10:25:28 +0200

    removing unnecessary files

--- catkin_ws/build/aroma_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/aroma/catkin_ws/src/aroma_cam
-# Build directory: /home/pi/aroma/catkin_ws/build/aroma_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
