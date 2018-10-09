f1c9dc226395b4fb6e4f1102d330a681068223f9
nifunk <nfunk@ethz.ch>
2018-10-09 22:31:04 +0200

    added feedback from airpump

--- catkin_ws/src/aroma_airpump/src/airpump.py
+++ catkin_ws/src/aroma_airpump/src/airpump.py
@@ -28,6 +28,10 @@ class AromaAirPump(object):
         self.sub_topic1 = '/aroma_airpump/control'
         self.subscriber1 = rospy.Subscriber(self.sub_topic1, String, self.callback_control,queue_size=1)
 
+        # FEEDBACK topic to show when being ready
+        self.pub_topic = '/aroma_airpump/control_ended'
+        self.publisher_end = rospy.Publisher(self.pub_topic, Bool, queue_size=1)
+
 
     # this is the callback function which processes the command from the outside and then executes
     # the needed or desired actions!!!
@@ -53,6 +57,7 @@ class AromaAirPump(object):
             time.sleep(period*(1-percentage))
             #print "BELLO CIAO"
         print ("Airpump cycle has ended!")
+        self.publisher_end.publish(True)
 
 
 
